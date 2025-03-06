c197cabb0917727839576a9c482396f26babaf93
windigal <2523604800@qq.com>
2025-03-06 20:56:26 +0800

    update the requiements

--- setup.py
+++ setup.py
@@ -5,19 +5,14 @@ from setuptools import find_packages, setup
 long_description = (Path(__file__).parent / "README.md").read_text()
 
 core_requirements = [
-    "gymnasium==0.29.1",
     "rich==13.7.1",
-    "tqdm==4.66.4",
     "ipdb==0.13.13",
-    "mujoco==3.1.6",
     "mujoco-mjx==3.1.6",
-    "dm_control==1.0.20",
-    "imageio==2.34.2",
     "gymnax==0.0.8",
     "brax==0.9.4",
-    "torch==2.3.1",
     "opencv-python==4.10.0.84",
     "natsort==8.4.0",
+    "jax[cuda12]==0.4.28",
 ]
 
 setup(
